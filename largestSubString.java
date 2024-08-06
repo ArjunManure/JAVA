467fa27f470ec9a33028817b6397df75f73d48c4
Manure Arjun <113349576+ArjunManure@users.noreply.github.com>
2024-08-06 23:29:22 +0530

    Update and rename largestSubStringPalindrom.java to largestSubString.java

--- largestSubString.java
+++ largestSubString.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class largestSubStringPalindrom {
+public class largestSubString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the binary string:");
